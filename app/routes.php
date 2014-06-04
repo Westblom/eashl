e842e9aaa4ca3ac5ba29adc148f2e549acc9bba5
Simon Kristiansson <simonkristiansson91@gmail.com>
2014-06-04 22:59:49 +0200

    adding auth filter

--- app/routes.php
+++ app/routes.php
@@ -15,8 +15,8 @@
 Route::get('/', ['as' => 'home', 'uses' => 'HomeController@showWelcome']);
 
 
-Route::get('/store', ['as' => 'store', 'uses' => 'StoreController@showStore']);
-Route::get('/store/{id}', ['as' => 'store.buy', 'uses' => 'StoreController@buyPack']);
+Route::get('/store', ['before' => 'auth' ,'as' => 'store', 'uses' => 'StoreController@showStore']);
+Route::get('/store/{id}', ['before' => 'auth' ,'as' => 'store.buy', 'uses' => 'StoreController@buyPack']);
 
 
 Route::get('/login', ['before' => 'guest', 'as' => 'login', 'uses' => 'UserController@showLogin']);
@@ -27,9 +27,9 @@ Route::get('/logout', ['as' => 'logout', 'uses' => 'UserController@logout']);
 Route::get('/signup', ['before' => 'guest', 'as' => 'signup', 'uses' => 'UserController@showSignup']);
 Route::post('/signup', ['before' => 'csrf', 'as' => 'signup.post', 'uses' => 'UserController@postSignup']);
 
-Route::get('/dashboard', ['as' => 'dashboard', 'uses' => 'DashboardController@showDashboard']);
-Route::get('/dashboard/newcards', ['as' => 'dashboard.newcards', 'uses' => 'DashboardController@newCards']);
-Route::get('/dashboard/team', ['as' => 'dashboard.team', 'uses' => 'DashboardController@showTeam']);
-Route::get('/dashboard/team/equip/{slot}/{card}', ['as' => 'dashboard.team.equip', 'uses' => 'DashboardController@postEquip']);
+Route::get('/dashboard', ['before' => 'auth' ,'as' => 'dashboard', 'uses' => 'DashboardController@showDashboard']);
+Route::get('/dashboard/newcards', ['before' => 'auth' ,'as' => 'dashboard.newcards', 'uses' => 'DashboardController@newCards']);
+Route::get('/dashboard/team', ['before' => 'auth' ,'as' => 'dashboard.team', 'uses' => 'DashboardController@showTeam']);
+Route::get('/dashboard/team/equip/{slot}/{card}', ['before' => 'auth' ,'as' => 'dashboard.team.equip', 'uses' => 'DashboardController@postEquip']);
 
 
